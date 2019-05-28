2eeb0b95f8a8c9fbbb54836c97ad26501c39d23a
Matthieu Lallai <matthieu.lallai@gmail.com>
2019-05-28 11:38:51 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO AGAIN !</div>;
+  return <div className="App">HELLO AGAIN ! What's your name ?</div>;
 }
 
 export default App;
